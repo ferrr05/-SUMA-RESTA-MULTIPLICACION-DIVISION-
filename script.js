334efbd21009db5ac5a7aab65b252916de9bc1bf
ferrr05 <123724312+ferrr05@users.noreply.github.com>
2023-04-13 10:55:00 +0200

    a

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let num1 = parseFloat(prompt("Ingrese el primer número: "));
-let num2 = parseFloat(prompt("Ingrese el segundo número: "));
-
-let suma = num1 + num2;
-let resta = num1 - num2;
-let multiplicacion = num1 * num2;
-let division = num2 !== 0 ? num1 / num2 : "No se puede dividir por cero.";
-
-console.log("El resultado de la suma es:", suma);
-console.log("El resultado de la resta es:", resta);
-console.log("El resultado de la multiplicación es:", multiplicacion);
-console.log("El resultado de la división es:", division);
+function calcular() {
+    var num1 = parseFloat(prompt("Introduzca el primer número:"));
+    var num2 = parseFloat(prompt("Introduzca el segundo número:"));
+    
+    var suma = num1 + num2;
+    var resta = num1 - num2;
+    var multiplicacion = num1 * num2;
+    var division = num1 / num2;
+    
+    alert("La suma es: " + suma + "\n" +
+          "La resta es: " + resta + "\n" +
+          "La multiplicación es: " + multiplicacion + "\n" +
+          "La división es: " + division);
+  }
