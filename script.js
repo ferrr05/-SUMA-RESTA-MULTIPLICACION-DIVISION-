44619cbfac81480d0815b9eabf7702471a92549f
ferrr05 <123724312+ferrr05@users.noreply.github.com>
2023-04-13 10:40:28 +0200

    TRABAJO TERMINADO

---
+++ script.js
@@ -0,0 +1,12 @@
+numero1 = float(input("Por favor, ingrese el primer número: "))
+numero2 = float(input("Por favor, ingrese el segundo número: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("La resta de", numero1, "y", numero2, "es:", resta)
+print("La multiplicación de", numero1, "y", numero2, "es:", multiplicacion)
+print("La división de", numero1, "y", numero2, "es:", division)
